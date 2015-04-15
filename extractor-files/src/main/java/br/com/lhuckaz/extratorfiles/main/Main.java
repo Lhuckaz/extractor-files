c7952446bf7df73b188da065b472e72da177a6d7
Lucas <lucasldaf12@yahoo.com.br>
2015-04-15 16:42:30 -0300

    Adicionando classes principais

---
+++ extractor-files/src/main/java/br/com/lhuckaz/extratorfiles/main/Main.java
@@ -0,0 +1,10 @@
+package br.com.lhuckaz.extratorfiles.main;
+
+/**
+ * @author Lucas
+ */
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ extractor-files/src/test/java/br/com/lhuckaz/extratorfiles/main/MainTest.java
@@ -0,0 +1,25 @@
+package br.com.lhuckaz.extratorfiles.main;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Teste simples
+ * 
+ * @author Lucas
+ */
+public class MainTest extends TestCase {
+
+	public MainTest(String testName) {
+		super(testName);
+	}
+
+	public static Test suite() {
+		return new TestSuite(MainTest.class);
+	}
+
+	public void testApp() {
+		assertTrue(true);
+	}
+}
