082d219426a27f58e71b2d413f32610289a7fc39
Lucas <lucasldaf12@yahoo.com.br>
2015-04-28 10:14:11 -0300

    Implementado funcao para abrir uma unica vez o programa

--- extractor-files/src/main/java/br/com/lhuckaz/extractorfiles/main/Main.java
+++ extractor-files/src/main/java/br/com/lhuckaz/extractorfiles/main/Main.java
@@ -1,7 +1,13 @@
 package br.com.lhuckaz.extractorfiles.main;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
+import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
+
+import org.apache.log4j.Logger;
 
 import br.com.lhuckaz.extractorfiles.gui.JExtratorFiles;
 
@@ -9,17 +15,25 @@ import br.com.lhuckaz.extractorfiles.gui.JExtratorFiles;
  * @author Lucas
  */
 public class Main {
+	private static Logger logger = Logger.getLogger(Main.class);
+	public static final int INSTANCIA_UNICA = 9581;
+	private static ServerSocket instancia;
 
 	public static void main(String[] args) throws Exception {
-		// Trocando o visual de acordo com o SO
-		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		// Rodar em outra Thread
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				new JExtratorFiles();
-			}
-		});
-
+		try {
+			instancia = new ServerSocket(INSTANCIA_UNICA);
+			logger.debug("Aplicacao aberta no socket: " + INSTANCIA_UNICA);
+			// Trocando o visual de acordo com o SO
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			// Rodar em outra Thread
+			SwingUtilities.invokeLater(new Runnable() {
+				public void run() {
+					new JExtratorFiles();
+				}
+			});
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(null, "Aplicação já aberta em outra janela", "Mensagem", JOptionPane.INFORMATION_MESSAGE);
+			logger.info("Aplicação já aberta em outra janela");
+		}
 	}
-
 }
